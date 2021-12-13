18e26aad3b2788bbfe79b8aec82d939143964a08
w40141 <w.40141@gmail.com>
2021-12-13 22:53:27 +0900

    add functions in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::path::PathBuf;
+use regex::Regex;
+use std::{fs::read_to_string, path::PathBuf};
 use structopt::StructOpt;
 
 /// Manage tags written in comments in files
 #[derive(StructOpt, Debug)]
 #[structopt(name = "srgat", about = "tag search for source code")]
-struct Opt {
+pub struct Cli {
     /// Pring tags in the files
     #[structopt(short, long, parse(from_os_str))]
     files: Option<Vec<PathBuf>>,
@@ -22,19 +23,67 @@ struct Opt {
     // dump: Option<PathBuf>,
 }
 
-fn run_args(opt: Opt) {
-    // TODO: すっきりさせたい。
-    match opt.files {
-        Some(v) => run_files(v),
-        None => match opt.directory {
-            Some(v) => run_dirctory(v, opt.ignore),
-            None => run_show(),
-        },
+fn find_tags(num: usize, line: &str) {
+    let tags = [
+        "TODO: ",
+        "INFO: ",
+        "FIX: ",
+        "WARNING: ",
+        "NOTE: ",
+        "HACK: ",
+        "PERF: ",
+    ];
+    for tag in tags {
+        if find_tag(line, tag) {
+            print_line(num, line)
+        }
     }
 }
 
+fn find_tag(line: &str, tag: &str) -> bool {
+    let re = Regex::new(tag).unwrap();
+    let contains_tag = re.find(line);
+    match contains_tag {
+        Some(_) => true,
+        None => false,
+    }
+}
+
+#[test]
+fn test_find_tag() {
+    let result = find_tag("TODO: This is a todo.", "TODO: ");
+    assert_eq!(result, true);
+    let result = find_tag("NOTE: This is a NOTE.", "NOTE: ");
+    assert_eq!(result, true);
+}
+
 fn run_files(v: Vec<PathBuf>) {
-    println!("{:#?}", v);
+    read_files(v)
+}
+
+fn read_files(vp: Vec<PathBuf>) {
+    for v in vp {
+        read_file(v);
+        println!("");
+    }
+    // NOTE: 改行
+}
+
+fn read_file(p: PathBuf) {
+    println!("{:?}", p);
+    let content = read_to_string(&p).expect("could not read file");
+    read_lines(content)
+}
+
+fn read_lines(content: String) {
+    for (i, line) in content.lines().enumerate() {
+        let num = i + 1;
+        find_tags(num, line)
+    }
+}
+
+fn print_line(num: usize, line: &str) {
+    println!("{}: {}", num, line);
 }
 
 fn run_dirctory(dir: String, ignore: Option<String>) {
@@ -44,11 +93,23 @@ fn run_dirctory(dir: String, ignore: Option<String>) {
     }
 }
 
+// TODO: showをerror表示から変える
 fn run_show() {
-    println!("show")
+    println!("error")
+}
+
+// TODO: すっきりさせる
+fn run(cli: Cli) {
+    match cli.files {
+        Some(v) => run_files(v),
+        None => match cli.directory {
+            Some(v) => run_dirctory(v, cli.ignore),
+            None => run_show(),
+        },
+    }
 }
 
 fn main() {
-    let opt = Opt::from_args();
-    run_args(opt)
+    let cli = Cli::from_args();
+    run(cli)
 }
